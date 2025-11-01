06122cafb06d399c9da792dd3db84ca8ef3c4623
Shreyansh Tiwari <shreyanshtiwari52@gmail.com>
2025-11-01 21:23:20 +0530

    users onboarding api done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 const app= express();
