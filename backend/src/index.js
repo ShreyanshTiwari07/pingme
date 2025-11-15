2596904b104486c9643cdc28e654e3a6358d27fa
Shreyansh Tiwari <shreyanshtiwari52@gmail.com>
2025-11-15 16:50:43 +0530

    chat message backend integration

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser"
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 const app= express();
 dotenv.config();
@@ -9,6 +10,7 @@ const PORT= process.env.PORT;
 app.use(express.json())
 
 app.use("/api/auth", authRoutes);
+app.use("/api/auth", messageRoutes);
 app.listen(PORT, ()=>{
     console.log('Server running on port ' + PORT)
     connectDB();
